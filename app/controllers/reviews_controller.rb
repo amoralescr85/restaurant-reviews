18726b7c6b2e31e7e7370907ba78bc6ab4fa38e8
amoralescr85 <amoralescr85@gmail.com>
2017-04-15 13:39:12 -0400

    added a board class

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+    if @review.save
+      flash[:notice] = "Review added successfully!"
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      flash[:error] = @review.errors.full_messages.join(". \n")
+      render :new
+    end
+  end
+
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :body).merge(restaurant: Restaurant.find(params[:restaurant_id]))
+  end
+
+end
